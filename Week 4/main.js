932db28be167d58423fe82f9279707ed6d568c20
Bill Burton <38678976+wbill122@users.noreply.github.com>
2020-10-10 21:18:07 -0700

    week 4

---
+++ Week 4/main.js
@@ -0,0 +1,22 @@
+const form = document.forms['hero'];
+form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+   
+   
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.realName = form.realName.value;
+    
+    
+
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialogform.powers;
+
+
+    
+
+   
+    }
+
+
